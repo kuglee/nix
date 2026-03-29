f5ae0b9671eee42a6571f0f5e61f02623f906152
Gábor Librecz <kuglee@gmail.com>
2026-03-29 21:46:08 +0200

    Add GUI apps

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
 
   outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
   let
-    configuration = { pkgs, ... }: {
+    configuration = { config, pkgs, ... }: {
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
       environment.systemPackages =
@@ -31,7 +31,36 @@
           pkgs.elmPackages.elm
           pkgs.elmPackages.elm-format
           pkgs.elmPackages.elm-optimize-level-2
+
+          # GUI apps
+          pkgs.brave
+          pkgs.jetbrains.idea
+          pkgs.keka
+          pkgs.mkvtoolnix
+          pkgs.transmission_4
+          # pkgs.darwin.xcode
         ];
+
+      nixpkgs.config.allowUnfree = true;
+      system.activationScripts.applications.text = let
+        env = pkgs.buildEnv {
+          name = "system-applications";
+          paths = config.environment.systemPackages;
+          pathsToLink = ["/Applications"];
+        };
+      in
+        pkgs.lib.mkForce ''
+          # Set up applications.
+          echo "setting up /Applications..." >&2
+          rm -rf /Applications/Nix\ Apps
+          mkdir -p /Applications/Nix\ Apps
+          find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+          while read -r src; do
+            app_name=$(basename "$src")
+            echo "copying $src" >&2
+                    ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+          done
+        '';
 
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
