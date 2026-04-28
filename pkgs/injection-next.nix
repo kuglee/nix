1155cfaf6d81018afc432567f34912d02493903d
Gábor Librecz <kuglee@gmail.com>
2026-04-28 19:23:25 +0200

    Apps: update injection-next

--- pkgs/injection-next.nix
+++ pkgs/injection-next.nix
@@ -1,11 +1,11 @@
 { mkMacApp }:
 
 let
-  version = "1.6.0";
+  version = "2.0.0RC6";
 in
 mkMacApp {
   pname = "injection-next";
   inherit version;
   url = "https://github.com/johnno1962/InjectionNext/releases/download/${version}/InjectionNext.zip";
-  hash = "sha256-6LDKGzzp+M/zOr91QoxkQmvVSvMmGNmUDQGsW7CqbCM=";
+  hash = "sha256-wh4zdPA6b4PJ1GQ5k70dqw0vq1M5Nr+KM8+/lM4ilTo=";
 }
