089680f335dfe8e52df9b6577eb3e6d852d56b1b
Gábor Librecz <kuglee@gmail.com>
2026-08-29 21:09:29 +0200

    Yazi: format config file

--- home/yazi.nix
+++ home/yazi.nix
@@ -1,11 +1,11 @@
 {
   programs.yazi = {
     enable = true;
-      settings = {
-        mgr = {
-          sort_dir_first = false;
-        };
-        preloaders = [];
+    settings = {
+      mgr = {
+        sort_dir_first = false;
       };
+      preloaders = [];
+    };
   };
 }
