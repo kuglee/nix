517c815e7ab2248a63092367655388fd40107761
Gábor Librecz <kuglee@gmail.com>
2026-08-29 21:12:00 +0200

    Yazi: add tokyinight theme

--- home/yazi.nix
+++ home/yazi.nix
@@ -1,6 +1,24 @@
+{ pkgs, ... }:
+
+let
+  tokyoNightRepo = pkgs.fetchFromGitHub {
+    owner = "kalidyasin";
+    repo = "yazi-flavors";
+    rev = "bfd1b37";
+    hash = "sha256-j9MCWSK8Xnj3mlbeQZcxkZ+wWQU+CCJfS39Tp8zextE="; 
+  };
+in
 {
   programs.yazi = {
     enable = true;
+    flavors = {
+      tokyonight-night = tokyoNightRepo + "/tokyonight-night.yazi";
+      tokyonight-day = tokyoNightRepo + "/tokyonight-day.yazi";
+    };
+    theme.flavor = {
+      dark  = "tokyonight-night";
+      light = "tokyonight-day";
+    };
     settings = {
       mgr = {
         sort_dir_first = false;
