b642c243f37061694b65adcd8d77d5c55fe61d27
Gábor Librecz <kuglee@gmail.com>
2026-09-15 10:25:40 +0200

    KeyboardLayout: create Keyboard Layouts directory if it doesn't exist

--- home/keyboard-layout.nix
+++ home/keyboard-layout.nix
@@ -3,6 +3,7 @@
 {
   # A relogin is needed for the keyboard layout to activate
   home.activation.copyKeyboardLayout = ''
+    mkdir -p ~/Library/Keyboard\ Layouts
     cp -f ${./keyboard-layout.keylayout} ~/Library/Keyboard\ Layouts/keyboard-layout.keylayout
   '';
 }
